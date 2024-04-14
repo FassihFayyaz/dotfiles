71aaef5ca924d22396f85c8c733228552a5e6a8e
Fassih Fayyaz <fassihfayyaz@gmail.com>
2024-04-14 20:48:24 +0500

    added corectrl to autostart with my profile

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#   ___ _____ ___ _     _____   ____  _             _    
-#  / _ \_   _|_ _| |   | ____| / ___|| |_ __ _ _ __| |_  
-# | | | || |  | || |   |  _|   \___ \| __/ _` | '__| __| 
-# | |_| || |  | || |___| |___   ___) | || (_| | |  | |_  
-#  \__\_\|_| |___|_____|_____| |____/ \__\__,_|_|   \__| 
-#                                                        
+#   ___ _____ ___ _     _____   ____  _             _
+#  / _ \_   _|_ _| |   | ____| / ___|| |_ __ _ _ __| |_
+# | | | || |  | || |   |  _|   \___ \| __/ _` | '__| __|
+# | |_| || |  | || |___| |___   ___) | || (_| | |  | |_
+#  \__\_\|_| |___|_____|_____| |____/ \__\__,_|_|   \__|
+#
 
 # My screen resolution
 # xrandr --rate 120
 
-# For Virtual Machine 
+# For Virtual Machine
 # xrandr --output Virtual-1 --mode 1920x1080
 
 # Start Polkit
@@ -27,12 +27,13 @@ redshift -l 55.7:12.6 &
 
 # Load power manager
 #xfce4-power-manager &
+corectrl --toggle-manual-profile Gaming --minimize-systray &
 
 # Load notification service
 dunst &
 
 # Setup Wallpaper and update colors
-#sh ~/.config/qtile/scripts/wal.sh
+sh ~/.config/qtile/scripts/wal.sh
 
 # Start NoiseTorch on Collar Mic
 noisetorch -i alsa_input.pci-0000_07_00.6.analog-stereo &
