6c65310cf58f202d5b62507fc290f119aaa41574
FassihFayyaz <fassihfayyaz@gmail.com>
2024-05-19 01:56:01 +0500

    fixed wofi in wallpaper select

--- .config/hypr/scripts/WallpaperSelect.sh
+++ .config/hypr/scripts/WallpaperSelect.sh
@@ -25,7 +25,7 @@ RANDOM_PIC="${PICS[$((RANDOM % ${#PICS[@]}))]}"
 RANDOM_PIC_NAME="${#PICS[@]}. random"
 
 # Rofi command
-wofi_command="wofi -i -show -dmenu -config ~/.config/wofi/config.rasi"
+wofi_command="wofi -i --show dmenu -config ~/.config/wofi/config.rasi"
 
 menu() {
   for i in "${!PICS[@]}"; do
